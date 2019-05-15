393f1c88280ab63114e8e1674d6d761c57033648
Angel Santos Ramirez <i72saraf@uco.es>
2019-05-15 13:27:30 +0200

    2

--- eje2.sh
+++ eje2.sh
@@ -1,10 +1,14 @@
-for x in $(ls -a HOME | grep '^\.')
+for x in $(ls -a /home/i72saraf | grep '^\.')
 	do 
 		numchar=$(echo $x | wc -m)
 		echo "$x $numchar"
-	done | sort -k2 -n | grep -o -E '^\..* '
+done | sort -k2 -n | grep -o -E '^\..* '
 
+proc=$(ps | sed -r -n '2,$ s/(^[0-9])+[ ]/p')
 
-grep -v '^ *$' > fich
+echo"$proc"
 
-ps xm
+#grep -v '^ *$' > fich
+
+#ps xm
+#+([0-9a-z/])+[ ]+([0-9:])+[ ]+([0-9a-z\-])
